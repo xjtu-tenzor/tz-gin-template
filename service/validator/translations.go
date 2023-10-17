1fcc16f10e228d841fd85eed012c38d6fa8518fb
libowen <libowen@tiaozhan.com>
2023-10-17 20:12:23 +0800

    fix: fix function name

--- service/validator/translations.go
+++ service/validator/translations.go
@@ -4,6 +4,6 @@ import (
 	ut "github.com/go-playground/universal-translator"
 )
 
-func TimingTranslate(ut ut.Translator) error {
+func categoryTransZh(ut ut.Translator) error {
 	return ut.Add("category", "{0}输入格式或长度不符(十位二进制)", true)
 }

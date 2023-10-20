5ad117cbb21bac0afe521dfc2bb60346d14f2aca
libowen <libowen@tiaozhan.com>
2023-10-20 23:19:34 +0800

    feat: update session operation

--- service/session.go
+++ service/session.go
@@ -22,12 +22,37 @@ func sessionGet(c *gin.Context, name string) interface{} {
 func sessionSet(c *gin.Context, name string, body interface{}) error {
 	session := sessions.Default(c)
 	if body == nil {
-		return errors.New("body is nil")
+		return ErrNew(errors.New("body is nil"), OpErr)
 	}
 	gob.Register(body)
 	session.Set(name, body)
+	return session.Save()
+}
+
+func sessionUpdate(c *gin.Context, name string, body interface{}) error {
+	if sessionGet(c, name) == nil {
+		return ErrNew(errors.New("cannot find session, Set please"), OpErr)
+	}
+	return sessionSet(c, name, body)
+}
+
+func sessionClear(c *gin.Context) error {
+	session := sessions.Default(c)
+	session.Clear()
 	if err := session.Save(); err != nil {
-		return err
+		return ErrNew(err, OpErr)
+	}
+	return nil
+}
+
+func sessionDelete(c *gin.Context, name string) error {
+	session := sessions.Default(c)
+	if sessionGet(c, name) == nil {
+		return errors.New("cannot find session, Set please")
+	}
+	session.Delete(name)
+	if err := session.Save(); err != nil {
+		return ErrNew(err, OpErr)
 	}
 	return nil
 }

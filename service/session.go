3f2baf8217ea3239fc26cdf61f25611c1b9edc09
libowen <libowen@tiaozhan.com>
2023-10-20 23:10:26 +0800

    feat: update session solve

--- service/session.go
+++ service/session.go
@@ -1,7 +1,33 @@
 package service
 
+import (
+	"encoding/gob"
+	"errors"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
 type UserSession struct {
 	ID       int
 	Username string
 	Level    int
+}
+
+func sessionGet(c *gin.Context, name string) interface{} {
+	session := sessions.Default(c)
+	return session.Get(name)
+}
+
+func sessionSet(c *gin.Context, name string, body interface{}) error {
+	session := sessions.Default(c)
+	if body == nil {
+		return errors.New("body is nil")
+	}
+	gob.Register(body)
+	session.Set(name, body)
+	if err := session.Save(); err != nil {
+		return err
+	}
+	return nil
 }

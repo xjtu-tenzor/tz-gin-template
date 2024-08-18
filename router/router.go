3cfef94a0a3bd5e7b96b0263471f5136d822fe65
asice <2492215094@qq.com>
2024-08-18 23:27:23 +0800

    remove:code for test

--- router/router.go
+++ router/router.go
@@ -1,11 +1,7 @@
 package router
 
 import (
-	"bytes"
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"io"
-	"net/http"
 	"template/middleware"
 )
 
@@ -20,30 +16,4 @@ func InitRouter(r *gin.Engine) {
 		apiRouter.GET("/time", ctr.Hello.HelloTime)
 		// end
 	}
-	v1 := apiRouter.Group("/v1")
-	{
-		v1.POST("/test", func(c *gin.Context) {
-			var req TestRequest
-			if c.Request.Body == nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "Request body is empty"})
-				return
-			}
-			bodyBytes, err := io.ReadAll(c.Request.Body)
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read request body"})
-				return
-			}
-			c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // 重置请求体
-			if err := c.ShouldBind(&req); err != nil {
-				logrus.Errorf("Binding error: %v", err)
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			c.JSON(http.StatusOK, gin.H{"message": "success"})
-		})
-	}
-}
-
-type TestRequest struct {
-	Name string `json:"name" binding:"required"`
 }

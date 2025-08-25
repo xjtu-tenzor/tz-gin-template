b6551d3ecf8d59e4a5cd4451466e67f913a26542
Asice Cloud <2492215094@qq.com>
2025-08-25 23:13:04 +0800

    fix master char

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 local_version=$(cat version.txt)
-remote_version=$(curl -s https://raw.githubusercontent.com/Asice-Cloud/tz-gin-template/main/version.txt)
+remote_version=$(curl -s https://raw.githubusercontent.com/Asice-Cloud/tz-gin-template/master/version.txt)
 if [ "$local_version" = "$remote_version" ]; then
   echo "Already latest version: $local_version"
 else
